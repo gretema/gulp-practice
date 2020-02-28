9c0c023a6f2032f85368040cdfa091a31b725c4e
gretema <jeanma1995@gmail.com>
2020-02-28 15:05:59 +0800

    Update 2020-02-28T07:04:53.845Z

---
+++ js/all.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var a = function a() {
+  return console.log('123');
+};
+
+var b = 1234;
+setTimeout(function () {
+  console.log('哈囉');
+}, 5000);
+"use strict";
+
+alert('abcd');
--- src/js/all.js
+++
@@ -1,7 +0,0 @@
-let a = () => console.log('123');
-
-const b = 1234;
-
-setTimeout(() => {
-  console.log('哈囉');
-}, 5000);
